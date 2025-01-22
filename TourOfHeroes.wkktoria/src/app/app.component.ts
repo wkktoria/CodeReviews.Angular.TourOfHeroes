7f869c8d58b1aad7e5f769e7a880c5096fbb8f24
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-01-22 20:17:29 +0100

    Create heroes component

--- TourOfHeroes.wkktoria/src/app/app.component.ts
+++ TourOfHeroes.wkktoria/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, HeroesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ TourOfHeroes.wkktoria/src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css',
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Batman',
+  };
+}
