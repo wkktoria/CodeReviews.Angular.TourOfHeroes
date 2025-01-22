091ca338d64cc122dd153b7c2182bcf0244b8f4f
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-01-22 20:25:01 +0100

    Display list of heroes

--- TourOfHeroes.wkktoria/src/app/heroes/heroes.component.ts
+++ TourOfHeroes.wkktoria/src/app/heroes/heroes.component.ts
@@ -1,18 +1,21 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgFor],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Batman',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ TourOfHeroes.wkktoria/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Batman' },
+  { id: 2, name: 'Spider-Mna' },
+  { id: 3, name: 'Superman' },
+  { id: 4, name: 'Wolverine' },
+  { id: 5, name: 'Iron Man' },
+  { id: 6, name: 'Captain America' },
+  { id: 7, name: 'Black Widow' },
+  { id: 8, name: 'Thor' },
+  { id: 9, name: 'Wonder Woman' },
+  { id: 10, name: 'Green Lantern' },
+];
