725f6c2c0e59883e22f72d7a93176cefeb409403
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-01-22 20:28:14 +0100

    Create hero's detail component

---
+++ TourOfHeroes.wkktoria/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgIf],
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css',
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+}
--- TourOfHeroes.wkktoria/src/app/heroes/heroes.component.ts
+++ TourOfHeroes.wkktoria/src/app/heroes/heroes.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgFor],
+  imports: [CommonModule, FormsModule, NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
