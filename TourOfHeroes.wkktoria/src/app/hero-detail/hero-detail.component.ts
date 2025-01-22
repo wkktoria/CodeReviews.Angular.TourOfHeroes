a2d37b138a5b67541932867dbb062a737d9bcbce
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-01-22 21:07:07 +0100

    Add navigation

--- TourOfHeroes.wkktoria/src/app/hero-detail/hero-detail.component.ts
+++ TourOfHeroes.wkktoria/src/app/hero-detail/hero-detail.component.ts
@@ -1,7 +1,9 @@
-import { CommonModule, NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { CommonModule, NgIf, Location } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,6 +12,25 @@ import { Hero } from '../hero';
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css',
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
 }
--- TourOfHeroes.wkktoria/src/app/heroes/heroes.component.ts
+++ TourOfHeroes.wkktoria/src/app/heroes/heroes.component.ts
@@ -6,17 +6,23 @@ import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgFor, HeroDetailComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgFor,
+    HeroDetailComponent,
+    RouterModule,
+  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(
     private heroService: HeroService,
@@ -25,11 +31,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
